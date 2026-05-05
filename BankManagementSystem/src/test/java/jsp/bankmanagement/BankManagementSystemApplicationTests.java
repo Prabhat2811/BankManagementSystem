29602b123320e78a5fbc8c3377276fcdc9a80202
Prabhat Ranjan <ranjanprabhat.c@gmail.com>
2026-05-05 09:42:44 +0530

    Initial Commit.

---
+++ BankManagementSystem/src/test/java/jsp/bankmanagement/BankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
